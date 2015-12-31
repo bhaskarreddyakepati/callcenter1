ab8ace34d383a9165aed0da3a265d6826ab1490c
bhaskarakepati <bhaskarakepati@gmail.com>
2015-12-31 07:42:06 +0000

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name callcenterApp
+ * @description
+ * # callcenterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('callcenterApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngMaterial',
+    'ngMessages'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name callcenterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the callcenterApp
+ */
+angular.module('callcenterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
